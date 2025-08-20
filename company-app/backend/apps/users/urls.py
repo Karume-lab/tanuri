82f862497c02cdde80f84f33193c236a262aba5b
Karume-lab <danielkarume@outlook.com>
2025-08-20 14:20:10 +0300

    chore: :fire: purge everything to try monorepo setup

--- company-app/backend/apps/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, re_path
-
-urlpatterns = [
-    re_path(r"^auth/", include("djoser.urls")),
-    re_path(r"^auth/", include("djoser.urls.jwt")),
-]
--- company-app/frontend/src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-
-const middleware = (req: NextRequest) => {
-  return NextResponse.next();
-};
-
-export default middleware;
