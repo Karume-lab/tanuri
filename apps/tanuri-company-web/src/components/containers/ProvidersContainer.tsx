2ef69568611b1103e746bed7431f41eab9dd2016
Karume-lab <danielkarume@outlook.com>
2025-08-31 10:24:49 +0300

    feat: :package: add more packages

--- apps/tanuri-company-web/src/components/containers/ProvidersContainer.tsx
+++ apps/tanuri-company-web/src/components/containers/ProvidersContainer.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 import type React from "react";
+import { useState } from "react";
 import { theme } from "@/styles/mantine-theme";
 
 interface ProviderContainerProps {
@@ -9,11 +12,23 @@ interface ProviderContainerProps {
 }
 
 const ProvidersContainer: React.FC<ProviderContainerProps> = ({ children }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
   return (
-    <MantineProvider theme={theme}>
-      <Notifications />
-      {children}
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider theme={theme}>
+        <Notifications />
+        <NuqsAdapter>{children}</NuqsAdapter>
+      </MantineProvider>
+    </QueryClientProvider>
   );
 };
 
